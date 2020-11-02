5cf629f12e2956c93ac22d2289e7a00a064cf8b7
sm9565 <70976372+sm9565@users.noreply.github.com>
2020-11-02 09:55:44 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -132,7 +132,7 @@ def ping(host, timeout=1):
     stdev_var = stdev(List)
     
 
-    vars = [str(round(packet_min)), str(round(packet_avg, 1)), str(round(packet_max)), str(round(stdev_var, 1))]
+    vars = [str(round(packet_min, 2)), str(round(packet_avg, 2)), str(round(packet_max, 2)), str(round(stdev_var, 2))]
 
     return vars
 
